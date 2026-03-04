c8a8924c7867988eff681adc4b216f60e84e1d32
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2026-03-04 23:52:47 +0100

    fixed deployment errors

--- src/pages/ReviewForm.tsx
+++ src/pages/ReviewForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ChevronLeft, Star, Send, Heart } from "lucide-react";
 
 const ReviewForm = () => {
